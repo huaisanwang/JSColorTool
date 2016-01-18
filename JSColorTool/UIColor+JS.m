ea1f1b83e318cd430d471cb572f2432cec1849d6
JsonWang <632494788@qq.com>
2016-01-18 14:51:19 +0800

    添加分类文件,具体处理16进制颜色问题;

--- JSColorTool/UIColor+JS.m
+++ JSColorTool/UIColor+JS.m
@@ -15,10 +15,44 @@
 
 +(UIColor *)colorWithJsString:(NSString *)colorStr
 {
-    return nil;
+    return [self colorWithJsString:colorStr alpha:1.0];
 }
 +(UIColor *)colorWithJsString:(NSString *)colorStr alpha:(CGFloat)alpha
 {
-    return nil;
+    //删除字符串中的空格
+    NSString *cString =[colorStr stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if([cString length] < 6 || [cString length] > 8){
+        return [UIColor clearColor];
+    }
+    if ([cString hasPrefix:@"0x"]) {
+        cString = [cString substringFromIndex:2];
+    }
+    if ([cString hasPrefix:@"#"]) {
+        cString =[cString substringFromIndex:1];
+    }
+    if ([cString length] !=6) {
+        return [UIColor clearColor];
+    }
+    
+    //字符转换
+    NSRange range;
+    range.location=0;
+    range.length=2;
+    //r
+    NSString *rStr = [cString substringWithRange:range];
+    //g
+    range.location=2;
+    NSString *gStr =[cString substringWithRange:range];
+    //b
+    range.location =4;
+    NSString *bStr =[cString substringWithRange:range];
+    
+    unsigned int r,g,b;
+    [[NSScanner scannerWithString:rStr] scanHexInt:&r];
+    [[NSScanner scannerWithString:gStr] scanHexInt:&g];
+    [[NSScanner scannerWithString:bStr] scanHexInt:&b];
+    
+    UIColor *color = [UIColor colorWithRed:((float)r/255.0f) green:((float)g/255.0f) blue:((float)b/255.0f) alpha:alpha];
+    return color;
 }
 @end
--- JSColorTool/ViewController.m
+++ JSColorTool/ViewController.m
@@ -4,7 +4,10 @@
 //
 //  Created by JsonWang on 16/1/18.
 //  Copyright © 2016年 JsonWang. All rights reserved.
-//
+//  (1)新建PCH文件,添加引用地址,如:#import "UIColor+JS.h"
+//  (2)配置PCH:在Build Setting->Prefix Header->项目名称/文件名+.pch,如:JSColorTool/JsColorPrefix.pch
+
+
 
 #import "ViewController.h"
 
@@ -16,10 +19,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-//    [UIColor colorWithRed:<#(CGFloat)#> green:<#(CGFloat)#> blue:<#(CGFloat)#> alpha:<#(CGFloat)#>]
-    
+    UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    rightButton.frame = CGRectMake(100, 100, 100, 44);
+    [rightButton setBackgroundColor:[UIColor colorWithJsString:@"#030303"]];
+    [rightButton setTitle:@"WHAT" forState:UIControlStateNormal];
+    [rightButton setTitleColor:[UIColor colorWithJsString:@"#708c3b"] forState:UIControlStateNormal];
+    rightButton.titleLabel.font = [UIFont fontWithName:@"Heiti SC" size:20.0f];
+//    [rightButton addTarget:self action:@selector(settingBtnPresss:)
+//          forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:rightButton];
 }
 
 - (void)didReceiveMemoryWarning {
