5e1f6fd05d2c106f0d06325e1688f4a8f4064e4f
JsonWang <632494788@qq.com>
2016-01-18 14:18:17 +0800

    添加新文件

---
+++ JSColorTool/UIColor+JS.m
@@ -0,0 +1,24 @@
+//
+//  UIColor+JS.m
+//  JSColorTool
+//
+//  Created by JsonWang on 16/1/18.
+//  Copyright © 2016年 JsonWang. All rights reserved.
+//
+
+
+
+#import "UIColor+JS.h"
+
+@implementation UIColor (JS)
+
+
++(UIColor *)colorWithJsString:(NSString *)colorStr
+{
+    return nil;
+}
++(UIColor *)colorWithJsString:(NSString *)colorStr alpha:(CGFloat)alpha
+{
+    return nil;
+}
+@end
